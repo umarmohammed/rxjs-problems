f37f4df13956656c48dc80c5cedabf129f8ccb97
Umar Mohammed <umar@thinkincode.co.uk>
2020-09-24 18:13:04 +0100

    wrap loading sideffects in doFetch and do cacheing

--- src/app/foo.service.ts
+++ src/app/foo.service.ts
@@ -1,19 +1,30 @@
 import { Injectable } from '@angular/core';
-import { timer, Subject, BehaviorSubject } from 'rxjs';
-import { take, map, switchMap, tap } from 'rxjs/operators';
+import { timer, Subject, BehaviorSubject, Observable, defer } from 'rxjs';
+import { take, map, switchMap, tap, shareReplay } from 'rxjs/operators';
 
 @Injectable({ providedIn: 'root' })
 export class FooService {
+  private cache = {};
   private fooSubject = new Subject<number>();
   fooLoading$ = new BehaviorSubject<boolean>(false);
   foo$ = this.fooSubject.asObservable().pipe(
-    tap(() => this.fooLoading$.next(true)),
-    switchMap(this.getFooById),
-    tap(() => this.fooLoading$.next(false))
+    switchMap((id) => {
+      if (!this.cache[id]) {
+        this.cache[id] = this.doFetch(this.getFooById(id)).pipe(shareReplay());
+      }
+      return this.cache[id];
+    })
   );
 
   fooSelected(id: number) {
     this.fooSubject.next(id);
+  }
+
+  doFetch(actualFetch$: Observable<string>) {
+    return defer(() => {
+      this.fooLoading$.next(true);
+      return actualFetch$.pipe(tap(() => this.fooLoading$.next(false)));
+    });
   }
 
   private getFooById(id: number) {
